4aae76463424de9840bf468d156c0fbe75df1b68
KazukiMoriai <moriaikazuki@gmail.com>
2024-10-23 10:23:51 +0900

    ファイル作成

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// jsを記述する際はここに記載していく
+
+// const alertmes =()=>{
+//     alert("HelloWorld");
+// }
+
+
+// const item = [1,2,3,4,5,6,7,8,9,10]
+// const result = item.filter(function(value){
+//     return value < 8
+// })
+
+// console.log(result);
+
+// const div = document.querySelector('div')
+// console.log(div.dataset.name)
